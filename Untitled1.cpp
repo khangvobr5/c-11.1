e5eb338920511aa75bd1b19b636bd00fcd124353
khangvobr5 <khangvobr5@gmail.com>
2024-12-25 21:17:49 +0700

    init project

---
+++ Untitled1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int tinhTong(int a, int b) {
+    return a + b;
+}
+int main() {
+    int x, y;
+    printf("Nhap so thu nhat: ");
+    scanf("%d", &x);
+    printf("Nhap so thu hai: ");
+    scanf("%d", &y);
+    int tong = tinhTong(x, y);
+    printf("Tong cua %d va %d la: %d\n", x, y, tong);
+    return 0;
+}
+
